f2d61657e61c6dc7968111ab20b114a7706b786b
zmartl <luca.marti@bluewin.ch>
2016-06-03 18:04:28 +0200

    Image added

---
+++ Vkazo/Vkazo.Droid/Resources/drawable-hdpi/CustomerPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Vkazo.Model;
+using Vkazo.Resources;
+using Xamarin.Forms;
+
+namespace Vkazo
+{
+    public partial class CustomerPage : ContentPage
+    {
+        public CustomerPage()
+        {
+            InitializeComponent();
+
+            CustomerListView.ItemsSource = new List<Customer>
+            {
+                new Customer
+                {
+                    Title = AppResources.ZurichZoo,
+                    Description = AppResources.ZurichZooDescription,
+                    DescriptionLong = AppResources.ZurichZooDescription,
+                    Image = AppResources.ImageZurichZoo
+                },
+                new Customer
+                {
+                    Title = AppResources.HinwilBadi,
+                    Description = AppResources.HinwilBadiDescription,
+                    DescriptionLong = AppResources.HinwilBadiDescription,
+                    Image = AppResources.ImageHinwilBadi
+                },
+                new Customer
+                {
+                    Title = AppResources.LiveAtSunset,
+                    Description = AppResources.LiveAtSunsetDescription,
+                    DescriptionLong = AppResources.LiveAtSunsetDescription,
+                    Image = AppResources.ImageLiveAtSunset
+                },
+                new Customer
+                {
+                    Title = AppResources.HinwilRocKTheRing,
+                    Description = AppResources.HinwilRocKTheRingDescription,
+                    DescriptionLong = AppResources.HinwilRocKTheRingDescription,
+                    Image = AppResources.ImageRockTheRing
+                }
+            };
+        }
+
+        public void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+            var detailPage = new CustomerDetailPage
+            {
+                Item = (Customer)e.SelectedItem
+            };
+            Navigation.PushAsync(detailPage);
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
